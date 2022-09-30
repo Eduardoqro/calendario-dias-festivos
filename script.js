fc1623d670f9394f7a4f7fc44edfb1a35c13e71f
Eduardoqro <eduardomendozaqro@gmail.com>
2022-09-30 02:54:23 -0500

    feat:script

---
+++ script.js
@@ -0,0 +1,10 @@
+function calcularFecha() {
+  const dayInput = document.getElementById("exampleFormControlInput1").value;
+  const monthInput = document.getElementById("exampleFormControlInput2").value;
+  const yearInput = document.getElementById("exampleFormControlInput3").value;
+
+  console.log({ dayInput, monthInput, yearInput });
+  console.log("ʕ•ᴥ•ʔっ")
+  }
+
+  console.log("ʕ•ᴥ•ʔっ")
