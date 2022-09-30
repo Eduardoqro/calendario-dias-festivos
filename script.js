6385ce345b2d4edb9f54ed7675933cd35fb3b6e4
Eduardoqro <eduardomendozaqro@gmail.com>
2022-09-30 18:36:37 -0500

    feat:calendarioV1

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
+const diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril','Mayo', 'Junio', 'Julio', 'Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
 function calcularFecha() {
-  const dayInput = document.getElementById("exampleFormControlInput1").value;
-  const monthInput = document.getElementById("exampleFormControlInput2").value;
-  const yearInput = document.getElementById("exampleFormControlInput3").value;
-
-  console.log({ dayInput, monthInput, yearInput });
-  console.log("ʕ•ᴥ•ʔっ")
+  // Valores de entrada
+  const dayInput = document.getElementById("dia").value;
+  const monthInput = document.getElementById("mes").value; //probar
+  const yearInput = document.getElementById("año").value;
+  // Obtener la fecha 
+  const date = new Date(yearInput, monthInput - 1, dayInput);
+  const dayIndex = date.getDay();
+  const mesIndex =date.getMonth();  //probar
+  const dayLabel = diasSemana[dayIndex]; 
+  const mesLabel=meses[mesIndex]; //probar
+console.log(mesLabel);
+  let message = "";
+  switch (dayIndex) {
+    case 0: case 6: // 0 al 6 debido al L a D
+      message = "No es dia laboral"; // Sabado y Domingo
+      break;
+    default: 
+      message = "Dia laboral"; // Serian L, M , M, J V
+      break;
   }
-
-  console.log("ʕ•ᴥ•ʔっ")
+  // Resultados en HTML y consola
+  const resultElement = document.getElementById('final'); // hacemos la conexion con HTML al input 
+  resultElement.value = `${dayLabel},${mesLabel},${message}`; // Se 
+  console.log(resultElement);
+  }
+  
